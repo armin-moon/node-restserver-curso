75e20aaa44ebfebbe1679315909ffb72f121f18f
armin.luna <armin.luna@next-technologies.com.mx>
2020-06-05 01:13:29 -0500

    RestServer -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,19 @@
 //puerto
 process.env.PORT = process.env.PORT || 3000;
+
+//=========
+//enterno
+//=========
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//base de datos
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://aluna:mRE32W6qlv8qguNo@cluster0-fgigd.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
